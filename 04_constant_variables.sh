5ef1fd8a3859b1693ebeb0cb028bd86a193c2e3e
Sunil Devareddy <sunildevareddy@Sunils-MacBook-Air.local>
2024-04-04 16:19:10 +0530

    example for constant variables

---
+++ 04_constant_variables.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+# this script shows how to declear a constant variable (means the value in the variabl will never change till the end of the script)
+readonly college=barath
+echo "my college name is $college"
+college=test #this second variable will not reflect in the out put this is call constent variable 
+
+
